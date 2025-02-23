686105ad32a899f5a259b2150cd71e13a2bbb26a
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-02-23 08:21:42 +0700

    New

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("Masukkan satu huruf: ");
+        char input = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if (input == 'A' || input == 'I' || input == 'U' || input == 'E' || input == 'O')
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf konsonan");
+        }
+    }
+}
